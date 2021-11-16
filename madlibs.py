f5a1705721ffeff7901d05e13020106739ba4973
Jen Nerby <nerby.jennifer@gmail.com>
2021-11-15 17:35:40 -0700

    Finished madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -54,12 +54,22 @@ def greet_person():
 @app.route("/game")
 def show_madlib_form():
     choice = request.args.get("game")
-    print(choice)
+
     if choice == "Yes":
         return render_template("game.html")
     else:
         return render_template("goodbye.html")
 
+        
+@app.route("/madlib")
+def show_madlib():
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+    return render_template("madlib.html", person=person, 
+                            color=color, noun=noun, adjective=adjective)
+                            
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
